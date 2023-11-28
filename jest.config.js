8d4d80b746c4c072c5d7dee0a047232570b65cf1
Hernán Domingo <hernan.domingo@hotmail.com>
2023-11-28 07:19:19 +0100

    test: add root folder to config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
