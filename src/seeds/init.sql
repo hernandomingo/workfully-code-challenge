3feeb6f762119c515b405db5b57d8be4b3424819
Hernán Domingo <hernan.domingo@hotmail.com>
2023-11-28 03:07:20 +0100

    feat(db): add createdAt and updatedAt columns

--- src/seeds/init.sql
+++ src/seeds/init.sql
@@ -1,9 +1,11 @@
 CREATE TABLE accounts (
     id SERIAL PRIMARY KEY,
-    balance float NOT NULL
+    balance float NOT NULL,
+    createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO accounts (id, balance, dailyDeposits) VALUES 
-(1, 100), 
-(2, 1000), 
-(3, 500);
+INSERT INTO accounts (id, balance, createdAt, updatedAt) VALUES 
+(1, 100, NOW(), NOW()), 
+(2, 1000, NOW(), NOW()), 
+(3, 500, NOW(), NOW());
